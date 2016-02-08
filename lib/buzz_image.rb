59d38875e9bd554c5672c94cb54e8b25c2086c77
Sohair <sohair1991@gmail.com>
2016-02-08 18:29:39 +0500

    Configuration: Added default values of hight width and level + provided options for rails app

--- lib/buzz_image.rb
+++ lib/buzz_image.rb
@@ -2,6 +2,7 @@ require "buzz_image/version"
 require "buzz_image/engine"
 require "buzz_image/buzz_image_helper"
 require "buzz_image/railtie"
+require "buzz_image/configuration"
 
 module BuzzImage
   # Your code goes here...
